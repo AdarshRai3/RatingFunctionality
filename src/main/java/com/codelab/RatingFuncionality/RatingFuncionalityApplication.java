a40b75324588ab4fef1fa9899b3278a80a224a38
AdarshRai3 <adarshaibusiness@gmail.com>
2024-10-16 12:56:26 +0530

    Initial commit

---
+++ src/main/java/com/codelab/RatingFuncionality/RatingFuncionalityApplication.java
@@ -0,0 +1,13 @@
+package com.codelab.RatingFuncionality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingFuncionalityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingFuncionalityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codelab/RatingFuncionality/RatingFuncionalityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelab.RatingFuncionality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingFuncionalityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
